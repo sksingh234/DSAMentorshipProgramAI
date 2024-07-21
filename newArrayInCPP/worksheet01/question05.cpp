5027b154d520b6e335594f66a84de11a6c967cd4
sksingh234 <shubhamkumar95817@gmail.com>
2024-07-21 19:14:16 +0530

    DSA mentorship st again

---
+++ newArrayInCPP/worksheet01/question05.cpp
@@ -0,0 +1,9 @@
+
+/*  Declare and initialize an array of 4 integers with values 1, 2, 3, and 4 in one statement.
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+       int arr[] = {1,2,3,4};
+}
