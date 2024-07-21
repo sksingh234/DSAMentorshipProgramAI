15c2eaa8706a0c57e861ef964d79491bcc01e925
sksingh234 <shubhamkumar95817@gmail.com>
2024-07-21 19:22:20 +0530

    print third elemeny

---
+++ newArrayInCPP/worksheet01/question06.cpp
@@ -0,0 +1,11 @@
+
+/*
+Given an array int arr[5] = {10, 20, 30, 40, 50};, write the code to access and print the third element.
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+     int arr[5] = {10, 20, 30, 40, 50};
+     cout<<arr[2];
+}
