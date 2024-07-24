33fcf06b27be858ea1991194426296874fca62ed
sksingh234 <shubhamkumar95817@gmail.com>
2024-07-24 11:10:20 +0530

    solu for sorting element using sort function

--- newArrayInCPP/vectorWorksheet01/ques05.cpp
+++ newArrayInCPP/vectorWorksheet01/ques05.cpp
@@ -2,3 +2,23 @@
 Implement a program that takes n integers as input from the user, stores them in a vector, and sorts the vector in ascending order using the sort function.
 */
 
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int n; 
+    cout<<" enter the value of n :";
+    cin>>n;
+    vector<int> v(n);
+    for (int i = 0; i < v.size(); i++)
+    {
+        cin>>v[i];
+    }
+    sort(v.begin(),v.end());
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout<<v[i]<<" ";
+    }
+}
